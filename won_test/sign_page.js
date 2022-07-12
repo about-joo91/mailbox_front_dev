9a24402c84c49af63c5dba650c586215a9785d88
heewon Choi <wonbb3313@gmail.com>
2022-07-13 08:27:07 +0900

    feat/로그인 페이지만들고 css수정, js header 수정

--- won_test/sign_page.js
+++ won_test/sign_page.js
@@ -1,5 +1,6 @@
-api.js
+
 const backend_base_url = "http://127.0.0.1:8000"
+
 async function SignUp(){
     const SignupData = {
         username : document.getElementById("Username").value,
@@ -8,6 +9,7 @@ async function SignUp(){
     }
     const response = await fetch(`${backend_base_url}/won_test/`, {
         headers : {
+            "Access-Control-Allow-Origin": "*",
             Accept: "application/json",
             "Content-type": "application/json"
         },
@@ -15,14 +17,16 @@ async function SignUp(){
         body: JSON.stringify(SignupData)
     })
     response_json = await response.json()
-    console.log(response_json)
+    
     if (response.status == 200){
-        console.log("성공!")
-        // window.location.replace(`${frontend_base_url}/login.html`);
+        alert("회원가입 완료!")
+        window.location.replace(`signin.html`);
     }else {
         alert(response.status)
     }
 }
+
+
 async function SignIn(){
     const SignupData = {
         username : document.getElementById("Username").value,
@@ -30,23 +34,21 @@ async function SignIn(){
     }
     const response = await fetch(`${backend_base_url}/won_test/api/token/`, {
         headers : {
+            "Access-Control-Allow-Origin": "*",
+            Accept: "application/json",
             "Content-type": "application/json"
         },
         method: "POST",
         body: JSON.stringify(SignupData)
     })
     response_json = await response.json()
-    console.log(response_json)
+
     if (response.status ==200){
+        alert("로그인 완료!")
         localStorage.setItem("access", response_json.access)
         localStorage.setItem("refresh", response_json.refresh)
-        const base64Url = response_json.access.split('.')[1];
-        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-        localStorage.setItem("payload", jsonPayload);
-        // window.location.replace(`${frontend_base_url}/`);
+        window.location.replace(`../jin_test/main_intro.html`);
+
     }else{
         alert(response.status)
     }
