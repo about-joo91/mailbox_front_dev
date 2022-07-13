7c84216edab29b3fbd31db4bc80bee3d286305fe
khw7876 <skanahrwi@naver.com>
2022-07-13 15:12:48 +0900

    feat/ko 백엔드와 연동하여 게시물 작성 구현

--- ko_test_board/board_page.js
+++ ko_test_board/board_page.js
@@ -1,3 +1,23 @@
+const BASE_URL = 'http://127.0.0.1:8000';
+
+// 쿠키 할당
+function get_cookie(name) {
+    let cookie_value = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookie_value = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookie_value;
+}
+const csrftoken = get_cookie('csrftoken')
+
+
 const modal_background = document.querySelector('.modal_background');
 const small_modal = document.querySelector('.small_modal');
 
@@ -9,16 +29,44 @@ if (e.target.classList.contains('modal_background')) {
 function open_modal(){
     document.querySelector('.modal_background').style.display="block"
     document.body.style.overflow = 'hidden';
-    console.log(small_modal.clientHeight)
     let modal_top_now = parseInt((window.innerHeight - small_modal.clientHeight) / 2)
     let modal_left_now = parseInt((window.innerWidth - small_modal.clientWidth) / 2)
     const small_modal_body = document.querySelector('.small_modal');
-    console.log(modal_top_now)
     small_modal_body.style.left = modal_left_now + "px";
     small_modal_body.style.top = modal_top_now + "px";
-
 }
  function close_modal(){
     document.querySelector('.modal_background').style.display="none"
     document.body.style.overflow = 'auto';
  }
+
+// 모달을 통해서 글을 작성 할 때 실행되는 코드
+
+ async function post_board(){
+    const post_title = document.querySelector(".sm_tt_title_input").value;
+    console.log(post_title)
+    const post_content = document.querySelector(".sm_bd_ct_textarea").value;
+    const token = localStorage.getItem('access')
+    const result = await fetch(BASE_URL + '/board/' ,{
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            "title": post_title,
+            "content" : post_content
+        })
+    })
+    let res = await result.json()
+    if (result.status == 200) {
+        alert("게시글을 작성 하였습니다!!")
+    }
+    else {
+        alert("게시글 작성에 실패하였습니다.")
+    }
+}
