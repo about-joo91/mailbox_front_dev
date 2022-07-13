f198ef4f78e0c307785749c9c421391a3be77406
khw7876 <skanahrwi@naver.com>
2022-07-13 17:22:00 +0900

    feat/ko 미완성 Get구동

--- ko_test_board/board_page.js
+++ ko_test_board/board_page.js
@@ -40,15 +40,109 @@ function open_modal(){
     document.body.style.overflow = 'auto';
  }
 
+ window.onload =
+    async function get_board() {
+        const result = await fetch(BASE_URL + '/board/' , {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrftoken,
+                'Authorization': "Bearer " + localStorage.getItem("access")
+            },
+        })
+        let res = await result.json()
+        if (result.status == 200) {
+            let tmp_board = ``
+            for (let i = 0; i < res.boards.length; i++){
+                // boards에 대한 제목, 내용, 이미지를 가져오는 코드
+                board = res.boards[i]
+                // 좋아요와 북마크의 상태를 가져오는 코드
+                    if(board.is_board_writer == true){
+                        tmp_board += `
+                    <div class="md_bb_bl_board" id="md_bb_bl_board_1">
+                        <div class="md_bb_bl_bd_description">
+                            <div class="md_bb_bl_bd_desc_image_icon"></div>
+                            <div class="md_bb_bl_bd_middle">
+                                <div class="md_bb_bl_bd_hidden_name">내가작성</div>
+                                <div class="md_bb_bl_bd_desc_create_date">${board.create_date}</div>
+                            </div>                         
+                            <div class="md_bb_bl_bd_desc_comment_icon">
+                                <i class="bi bi-chat-dots"></i>
+                                <div class="md_bb_bl_bd_desc_ci_comment_count">${board.board_comment.length}</div>
+                                <i class="bi bi-brightness-high id="bi-brightness-high_${board.id}"></i>
+                                <div class="md_bb_bl_bd_ct_right_sun_count">${board.like_count}</div>
+                            </div>
+                        </div>
+                        <div class="md_bb_bl_bd_content">
+                            <p class="md_bb_bl_bd_ct_left">
+                                ${board.content}
+                            </p>
+                            <div class="md_bb_bl_bd_ct_right">
+                                <div class="md_bb_bl_bd_ct_rg_border"></div>
+                            </div>
+                        </div>
+                    </div>`
+                    }
+                    else{
+                        tmp_board += `
+                    <div class="md_bb_bl_board" id="md_bb_bl_board_1">
+                        <div class="md_bb_bl_bd_description">
+                            <div class="md_bb_bl_bd_desc_image_icon"></div>
+                            <div class="md_bb_bl_bd_middle">
+                                <div class="md_bb_bl_bd_hidden_name">익명1</div>
+                                <div class="md_bb_bl_bd_desc_create_date">2022-07-07 07:07</div>
+                            </div>                         
+                            <div class="md_bb_bl_bd_desc_comment_icon">
+                                <i class="bi bi-chat-dots"></i>
+                                <div class="md_bb_bl_bd_desc_ci_comment_count">${board.board_comment.length}</div>
+                                <i class="bi bi-brightness-high" id="bi-brightness-high_${board.id}"></i>
+                                <div class="md_bb_bl_bd_ct_right_sun_count">${board.like_count}</div>
+                            </div>
+                        </div>
+                        <div class="md_bb_bl_bd_content">
+                            <p class="md_bb_bl_bd_ct_left">
+                                ${board.content}
+                            </p>
+                            <div class="md_bb_bl_bd_ct_right">
+                                <div class="md_bb_bl_bd_ct_rg_border"></div>
+                            </div>
+                        </div>
+                    </div>`
+                    }
+
+
+                const board_lists = document.querySelector(".mc_bb_board_lists")
+                const empty_sun = document.getElementById("bi-brightness-high_"+ board.id)
+                if(board.is_liked == true){
+                    empty_sun.classList.replace("bi-brightness-high", "bi-brightness-high-fill");
+                }
+                board_lists.innerHTML = tmp_board
+            }
+        }
+        else {
+            alert("세션이 만료 되었습니다.")
+            location.replace('/user/sign_page.html')
+        }
+    }
+
+
+
+
+
+
+
 // 모달을 통해서 글을 작성 할 때 실행되는 코드
 
- async function post_board(){
-    const post_title = document.querySelector(".sm_tt_title_input").value;
-    console.log(post_title)
-    const post_content = document.querySelector(".sm_bd_ct_textarea").value;
+ async function boards_board(){
+    const boards_title = document.querySelector(".sm_tt_title_input").value;
+    console.log(boards_title)
+    const boards_content = document.querySelector(".sm_bd_ct_textarea").value;
     const token = localStorage.getItem('access')
     const result = await fetch(BASE_URL + '/board/' ,{
-        method: 'POST',
+        method: 'boards',
         mode: 'cors',
         headers: {
             "Access-Control-Allow-Origin": "*",
@@ -58,8 +152,8 @@ function open_modal(){
             'Authorization': `Bearer ${token}`
         },
         body: JSON.stringify({
-            "title": post_title,
-            "content" : post_content
+            "title": boards_title,
+            "content" : boards_content
         })
     })
     let res = await result.json()
